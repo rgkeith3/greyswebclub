363dfc0386bb6360ae71f4ecd67da1c91e55f3b4
Grey Keith <r.grey.keith@gmail.com>
2017-05-23 03:02:30 -0700

    fix merge typo

--- app/frontend/actions/posts_actions.js
+++ app/frontend/actions/posts_actions.js
@@ -34,12 +34,6 @@ export const requestPosts = () => dispatch => (
 export const requestAllPosts = () => dispatch => (
   PostsApiUtil.fetchAllPosts()
     .then(posts => dispatch(receivePosts(posts)),
-      () => dispatch(receivePostErrors(["Couldn't retrieve posts"])))
-)
-
-export const requestAllPosts = () => dispatch => (
-  PostsApiUtil.fetchAllPosts()
-    .then(posts => dispatch(receivePosts(posts)),
       () => dispatch(recievePostErrors(["Couldn't retrieve posts"])))
 )
 
