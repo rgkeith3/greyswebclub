df21471361245e2194f9037995a2257cff8a38a8
Grey Keith <r.grey.keith@gmail.com>
2017-05-26 14:13:35 -0700

    Production Ready

--- Gemfile
+++ Gemfile
@@ -44,6 +44,8 @@ gem 'faker'
 # gem 'capistrano-rails', group: :development
 
 group :development, :test do
+  gem 'pry-rails'
+  gem 'spring'
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem 'byebug', platform: :mri
 end
--- app/frontend/store/store.js
+++ app/frontend/store/store.js
@@ -1,14 +1,13 @@
-import { createStore, applyMiddleware, compose } from 'redux'
+import { createStore, applyMiddleware } from 'redux'
 import thunk from 'redux-thunk'
 import rootReducer from '../reducers/root_reducer'
 
-const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose
 
 const configureStore = (preloadedState = {}) => (
   createStore(
     rootReducer,
     preloadedState,
-    composeEnhancers(applyMiddleware(thunk))
+    applyMiddleware(thunk)
   )
 )
 
